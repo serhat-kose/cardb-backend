858b0cb6cf40af107316f21f6d3d431d4d729c91
serhat-kose <serhatkse94@gmail.com>
2021-10-14 16:40:56 +0300

    edit backend

---
+++ web/web/src/main/java/com/serhat/web/WebApplication.java
@@ -0,0 +1,77 @@
+package com.serhat.web;
+
+import com.serhat.web.Entity.Car;
+import com.serhat.web.Entity.Owner;
+import com.serhat.web.Entity.User;
+import com.serhat.web.Repository.CarRepository;
+import com.serhat.web.Repository.OwnerRepository;
+import com.serhat.web.Repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebApplication {
+
+	private static  final Logger logger = LoggerFactory.getLogger(WebApplication.class);
+
+	@Autowired
+	private CarRepository carRepository;
+
+	@Autowired
+	private OwnerRepository ownerRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public static void main(String[] args) {
+
+		//Testing Your frontend page cahpter 13 test sonra bakılacak
+		SpringApplication.run(WebApplication.class, args);
+		logger.info(("hello spring boot"));
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+
+			// Add owner objects and save these to db
+			Owner owner1 = new Owner("John" , "Johnson");
+			Owner owner2 = new Owner("Mary" , "Robinson");
+
+			ownerRepository.save(owner1);
+			ownerRepository.save(owner2);
+
+			// Add car object with link to owners and save these to db.
+			Car car = new Car("Ford", "Mustang", "Red",
+					"ADF-1121", 2017, 59000, owner1);
+			carRepository.save(car);
+
+			car = new Car("Nissan", "Leaf", "White",
+					"SSJ-3002", 2014, 29000, owner2);
+			carRepository.save(car);
+
+			car = new Car("Toyota", "Prius", "Silver",
+					"KKO-0212", 2018, 39000, owner2);
+			carRepository.save(car);
+
+			// username: user password: user
+
+			userRepository.save(new User("user",
+					"$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi",
+					"USER"));
+// username: admin password: admin
+			userRepository.save(new User("admin",
+					"$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG",
+					"ADMIN"));
+
+
+
+		};
+	}
+
+}
